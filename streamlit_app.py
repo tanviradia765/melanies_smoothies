00261d7c000d6e50fc9f1c250c7658e73e61312d
tanviradia765 <tanvi.radia@lirik.io>
2025-01-24 20:49:07 +0530

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -8,37 +8,11 @@ st.write(
     """Choose the fruits you want in your custom Smoothie!
     """
 )
-
-from snowflake.snowpark.functions import col
-
 # session = get_active_session()
-# my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# # st.dataframe(data=my_dataframe, use_container_width=True)
 
-# ingredients_list = st.multiselect(
-#     'Choose upto 5 ingredients:'
-#     ,my_dataframe
-# )
+cnx = st.connection("snowflake")
+session = cnx.session()
 
-# if ingredients_list:
-#     ingredients_string = ''
-    
-#     for fruit_chosen in ingredients_list:
-#         ingredients_string += fruit_chosen + ' '
-        
-#         my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-#             values ('""" + ingredients_string + """')"""
-        
-#         time_to_insert = st.button('Submit Order')
-        
-#         if time_to_insert:
-#             session.sql(my_insert_stmt).collect()
-            
-#             st.success('Your Smoothie is ordered!', icon="✅")
-
-
-
-session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')).collect()
 
 # Convert the Snowflake DataFrame to a list of fruit names
