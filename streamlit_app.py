3f26d7eda3cc44162469c2653d928b56fb18c274
tanviradia765 <tanvi.radia@lirik.io>
2025-01-27 16:45:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,48 +17,49 @@ session = cnx.session()
 # my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')).collect()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+# st.stop()
 
 # Convert the snowpark dataframe to pandas dataframe so we can use LOC function
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 # Streamlit multiselect for up to 5 ingredients
 ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients:',
-    fruit_names
+    'Choose up to 5 ingredients:'
+    , my_dataframe
+    , max_selections=5
 )
 
-# Convert the Snowflake DataFrame to a list of fruit names
-fruit_names = [row['FRUIT_NAME'] for row in my_dataframe]
+# # Convert the Snowflake DataFrame to a list of fruit names
+# fruit_names = [row['FRUIT_NAME'] for row in my_dataframe]
 
-# Streamlit text input for the user's name on the order
-name_on_order = st.text_input("Enter your name for the order:")
-st.write("The name on your Smoothie will be ", name_on_order)
+# # Streamlit text input for the user's name on the order
+# name_on_order = st.text_input("Enter your name for the order:")
+# st.write("The name on your Smoothie will be ", name_on_order)
 
-# # Streamlit multiselect for up to 5 ingredients
-# ingredients_list = st.multiselect(
-#     'Choose up to 5 ingredients:',
-#     fruit_names
-# )
+# # # Streamlit multiselect for up to 5 ingredients
+# # ingredients_list = st.multiselect(
+# #     'Choose up to 5 ingredients:',
+# #     fruit_names
+# # )
 
-if ingredients_list and name_on_order:
-    # Create a single string of selected ingredients
-    ingredients_string = ' '.join(ingredients_list)
+# if ingredients_list and name_on_order:
+#     # Create a single string of selected ingredients
+#     ingredients_string = ' '.join(ingredients_list)
     
-    # Button for submitting the order
-    time_to_insert = st.button('Submit Order', key="submit_order_button")
+#     # Button for submitting the order
+#     time_to_insert = st.button('Submit Order', key="submit_order_button")
     
-    if time_to_insert:
-        # Insert the order into the database
-        my_insert_stmt = f"""
-        INSERT INTO smoothies.public.orders (ingredients, name_on_order)
-        VALUES ('{ingredients_string}', '{name_on_order}')
-        """
-        session.sql(my_insert_stmt).collect()
-        # st.success(f"Thank you {name_on_order}, your smoothie order has been placed!", icon="✅")
-        st.success(f"Your Smoothie is ordered, {name_on_order}", icon="✅")
+#     if time_to_insert:
+#         # Insert the order into the database
+#         my_insert_stmt = f"""
+#         INSERT INTO smoothies.public.orders (ingredients, name_on_order)
+#         VALUES ('{ingredients_string}', '{name_on_order}')
+#         """
+#         session.sql(my_insert_stmt).collect()
+#         # st.success(f"Thank you {name_on_order}, your smoothie order has been placed!", icon="✅")
+#         st.success(f"Your Smoothie is ordered, {name_on_order}", icon="✅")
 
 # New section to display smoothiefroot nutrition information
 if ingredients_list:
