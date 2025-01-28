fbc2281c95f4a66c94815e89c69bb8f917acb1a3
tanviradia765 <tanvi.radia@lirik.io>
2025-01-28 15:04:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -19,8 +20,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 fruit_names = [row['FRUIT_NAME'] for row in my_dataframe]
 
 # Streamlit text input for the user's name on the order
-name_on_order = st.text_input("Enter your name for the order:")
-st.write("The name on your Smoothie will be ", name_on_order)
+name_on_order = st.text_input("Name on Smoothie:")
+st.write("The name on your Smoothie will be: ", name_on_order)
 
 # Streamlit multiselect for up to 5 ingredients
 ingredients_list = st.multiselect(
@@ -42,13 +43,13 @@ if ingredients_list and name_on_order:
         VALUES ('{ingredients_string}', '{name_on_order}')
         """
         session.sql(my_insert_stmt).collect()
-        # st.success(f"Thank you {name_on_order}, your smoothie order has been placed!", icon="✅")
         st.success(f"Your Smoothie is ordered, {name_on_order}", icon="✅")
 
-
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-
-# Putting json into dataframe
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+if ingredients_list:
+    ingredients_string = ''
+    
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
